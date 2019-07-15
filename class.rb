629f319654cdc5f0ad9c46bc0c76919727b732f3
Joyce Mayaka <Joyce@JordanR-1808.310.hqraleigh.com>
2019-07-15 19:13:57 -0400

    Some commit

---
+++ class.rb
@@ -0,0 +1,8 @@
+puts "This is the classroom mangager"
+puts "It's just an example to get Git info started"
+puts "It does"
+
+puts "Here's a second paragraph"
+puts "Really, it's useless"
+puts "I'm just trying to illustrate points."
+
